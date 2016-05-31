a166327ba763c599d52b7287f46597029d21394d
Wataru Komiyama <qwertyuiop.w.k@gmail.com>
2016-05-31 18:52:36 +0900

    Javaからのデータベースアクセス　課題9

---
+++ challenge_12/kadai9.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databace_java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+/**
+ *
+ * @author Wataru
+ */
+public class kadai9 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        
+        request.setCharacterEncoding("UTF-8"); //文字コード定義
+        String id = request.getParameter("insertid"); //挿入フォーム
+        String name = request.getParameter("insertname"); //挿入フォーム
+        String tell = request.getParameter("inserttell"); //挿入フォーム
+        String age = request.getParameter("insertage"); //挿入フォーム
+        String birthday = request.getParameter("insertbirth"); //挿入フォーム
+        
+            Connection db_con = null;
+            PreparedStatement db_st = null;
+            
+            try
+            {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db","Kashiwagi","****");
+            
+            //SQL
+            db_st = db_con.prepareStatement("INSERT INTO profiles VALUES(?,?,?,?,?)");
+            db_st.setInt(1,Integer.parseInt(id)); //ID
+            db_st.setString(2,name); //名前
+            db_st.setString(3,tell); //電話番号
+            db_st.setInt(4,Integer.parseInt(age)); //年齢
+            db_st.setDate(5,new java.sql.Date(sdf.parse(birthday).getTime())); //生年月日
+            
+            int d = db_st.executeUpdate(); //データベースに書き込んだ件数
+                if(d != 0){    
+                    out.println("データベースに"+d+"件の書き込みを成功しました。");    
+                }
+            
+            db_st.close();
+            db_con.close();
+            } catch (SQLException e_sql) {
+                out.println("接続時にエラーが発生しました："+e_sql.toString());
+            } catch (Exception e) {
+                out.println("接続時にエラーが発生しました："+e.toString());
+            } finally {
+                if (db_con != null){
+                    try {
+                        db_con.close();
+                    } catch (Exception e_con) {
+                        System.out.println(e_con.getMessage());
+                    }
+                }
+                
+            }
+    }        
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ challenge_12/kadai9.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : kadai9
+    Created on : 2016/05/31, 17:08:48
+    Author     : Wataru
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+    <head>
+<html>        
+    <head>        
+        <title>kadai9</title>
+    </head>
+    <body>
+        <form method="get" action="/Database_Java/kadai9">
+    </body>
+    profileID:<INPUT type="text" name="insertid">
+    <br>
+    name:<INPUT type="text" name="insertname">
+    <br>
+    tell:<INPUT type="text" name="inserttell">
+    <br>
+    age:<INPUT type="text" name="insertage">
+    <br>
+    birthday:<INPUT type="text" name="insertbirth">
+    <br>
+    <input type="submit" value="送信">
+        </form>
+        
+</html>
